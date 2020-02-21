d6a875a783b5ed220f75c353bf835935d2705c04
guixu40 <2929067689@qq.com>
2020-02-21 20:53:33 +0800

    知识点1

---
+++ pages/home/home.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    name: 'guixu',
+    age: 18,
+    students: [
+      { id: 110, name: 'kobe', age: 30 },
+      { id: 111, name: 'james', age: 28 },
+      { id: 112, name: 'curry', age: 32 },
+      { id: 113, name: 'why', age: 18 }
+    ],
+    counter: 0
+  },
+  handleBtnClick() {
+    // 1.错误做法: 界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  }
+})
